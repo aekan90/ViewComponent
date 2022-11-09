d3cd8ce41ed9587db2b0e7f3531048c6fcbb9144
Atıf Ertugrul Kan <aekan90@hotmail.com>
2022-11-09 11:51:44 +0300

    Add project files.

---
+++ ViewComponent/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ViewComponent.Models;
+
+namespace ViewComponent.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var kitapList = new List<Kitaplar>()
+            {
+            new Kitaplar(){ Id=1,KitapAd="Kitap1",Yazar="Yazar1"},
+            new Kitaplar(){ Id=1,KitapAd="Kitap2",Yazar="Yazar2"},
+            new Kitaplar(){ Id=1,KitapAd="Kitap3",Yazar="Yazar3"},
+            new Kitaplar(){ Id=1,KitapAd="Kitap4",Yazar="Yazar4"},
+            new Kitaplar(){ Id=1,KitapAd="Kitap5",Yazar="Yazar5"},
+            };
+            return View(kitapList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ViewComponent/Models/Kitaplar.cs
@@ -0,0 +1,13 @@
+﻿namespace ViewComponent.Models
+{
+    public class Kitaplar
+    {
+        
+        public Kitaplar(){}
+
+        public int Id { get; set; }
+        public string KitapAd { get; set; }
+        public string Yazar { get; set; }
+
+    }
+}
